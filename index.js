b992ebdf8b6dafc05ace32c962afdd77133cbd2f
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-23 05:33:40 +0300

    Add set function

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 class HashMap {
   constructor(load_factor, capacity) {
-    load_factor = this.load_factor;
-    capacity = this.capacity;
+    this.load_factor = load_factor;
+    this.capacity = capacity;
+    this.buckets = new Array(capacity);
+  }
+
+  logBuckets() {
+    console.log(this.buckets);
   }
 
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + (key.charCodeAt(i) % this.capacity);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
-
     return hashCode;
   }
+
+  set(key, value) {
+    const index = this.hash(key);
+
+    if (this.buckets[index] === undefined || this.buckets[index] === null) {
+      const newBucket = new Array();
+      newBucket.push([key, value]);
+      this.buckets[index] = newBucket;
+    } else {
+      for (let i = 0; i < this.buckets[index].length; i++) {
+        if (this.buckets[index][i][0] === key) {
+          this.buckets[index][i][1] = value;
+          return;
+        }
+      }
+      this.buckets[index].push([key, value]);
+    }
+  }
 }
+
+const testMap = new HashMap();
+testMap.logBuckets();
